9b81ba05c820876875ff2fb47af62be1d67dd1f9
Mayowa Oyekan <z5592472@ad.unsw.edu.au>
2025-05-15 23:34:20 +1000

    edited so that the get limits function can work with the colour red

--- detect.py
+++ detect.py
@@ -3,7 +3,16 @@ import numpy as np
 
 from util import get_limits
 
-def get_mask(img, lower, upper):
+def get_mask(img, range):
+    mask = None
+    for lower, upper in range:
+        partial_mask = cv.inRange(img, lower, upper)
+        if mask is None:
+            mask = partial_mask
+        else:
+            mask = cv.bitwise_or(mask, partial_mask)
+
+
     return cv.inRange(img, lower, upper)
 
 def mask(blue, yellow, green, red, purple):
@@ -20,34 +29,28 @@ green = [0, 255, 0]
 while True:
 
     _, img = video.read()
-    img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    img = cv.GaussianBlur(img, (13, 13), 0)
+    # img = cv.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
 
     # BLUE and YELLOW are for road lines
     # RED and PURPLE obstacle detection draw rectangles maybe
     # GREEN is the end
 
-    # This is for BLUE
-    blue_lower, blue_upper = get_limits(color=blue)
+    blue_range = get_limits(blue)
+    yellow_range = get_limits(yellow)
+    red_range = get_limits(red)
+    purple_range = get_limits(purple)
+    green_range = get_limits(green)
 
-    # This is for YELLOW
-    yellow_lower, yellow_upper = get_limits(color=yellow)
+    mask_blue = get_mask(hsv_img, blue_range)
+    mask_yellow = get_mask(hsv_img, yellow_range)
+    
 
-    # This is for RED
-    red_lower, red_upper = get_limits(color=red)
-
-    # This is for PURPLE
-    purple_lower, purple_upper = get_limits(color=purple)
-
-    # This is for GREEN
-    green_lower, green_upper = get_limits(color=green)
-
-    mask_blue = get_mask(img, blue_lower, blue_upper)
-    mask_yellow = get_mask(img, yellow_lower, yellow_upper)
-
-    mask_red = get_mask(img, red_lower, red_upper)
-    mask_purple = get_mask(img, purple_lower, purple_upper)
-
-    mask_green = get_mask(img, green_lower, green_upper)
+        # lower mask (0-10)
+    mask_red = get_mask(hsv_img, red_range)
+    mask_purple = get_mask(hsv_img, purple_range)
+    mask_green = get_mask(hsv_img, green_range)
 
     master_mask = mask(mask_blue, mask_yellow, mask_red, mask_purple, mask_green)
 
@@ -58,7 +61,7 @@ while True:
     # contours, hieracrhy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     cv.imshow("mask", master_mask)
-    cv.imshow("webcam", res)
+    cv.imshow("webcam", img)
 
     # if len(contours) != 0 :
     #     for contour in contours:
--- util.py
+++ util.py
@@ -1,16 +1,44 @@
 import numpy as np
 import cv2 as cv
 
+# The function also deals with if red as that colour 'wraps' around the wheel
 def get_limits(color) :
 
     c = np.uint8([[color]])
     hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
 
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
+    h = hsvC[0][0][0]
 
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
+    if h < 10:
+        lowerLimit1 = 0, 100, 100
+        upperLimit1 = h + 10, 255, 255
+        lowerLimit2 = 180 + (h - 10), 100, 100
+        upperLimit2 = 180, 255, 255
 
+        lower1 = np.array(lowerLimit1, dtype=np.uint8)
+        upper1 = np.array(upperLimit1, dtype=np.uint8)
+        lower2 = np.array(lowerLimit2, dtype=np.uint8)
+        upper2 = np.array(upperLimit2, dtype=np.uint8)
+        
+        return [(lower1, upper1), (lower2, upper2)]
+    
+    if (h > 170):
+        lowerLimit1 = h - 10, 100, 100
+        upperLimit1 = 180, 255, 255
+        lowerLimit2 = 0, 100, 100
+        upperLimit2 =  (h + 10) - 180, 255, 255
 
-    return lowerLimit, upperLimit
+        lower1 = np.array(lowerLimit1, dtype=np.uint8)
+        upper1 = np.array(upperLimit1, dtype=np.uint8)
+        lower2 = np.array(lowerLimit2, dtype=np.uint8)
+        upper2 = np.array(upperLimit2, dtype=np.uint8)
+
+        return [(lower1, upper1), (lower2, upper2)]
+    else:
+
+        lowerLimit = h - 10, 100, 100
+        upperLimit = h + 10, 255, 255
+
+        lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+        upperLimit = np.array(upperLimit, dtype=np.uint8)
+        return [(lowerLimit, upperLimit)]
