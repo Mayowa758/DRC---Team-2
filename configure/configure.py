2d93f2392820ce5a52ebbbfa8e864744631aef33
Mayowa Oyekan <z5592472@ad.unsw.edu.au>
2025-05-25 22:49:45 +1000

    trying to fix configure for camera to fix fisheye

--- configure/configure.py
+++ configure/configure.py
@@ -2,63 +2,67 @@ import numpy as np
 import cv2 as cv
 import glob
 
-# from OpenCV website
-# termination criteria
+# Termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# Prepare object points
+objp = np.zeros((6*7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+# Arrays to store object and image points
+objpoints = []
+imgpoints = []
+gray_shape = None
 
-images = glob.glob('*.jpg')
+images = glob.glob('calib_images/*.jpg')
 
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+    if ret:
+        objpoints.append(objp.copy())
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
+        gray_shape = gray.shape[::-1]  # Save shape for calibration
 
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        # Optional: visualize detections
+        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
         cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.waitKey(200)
 
 cv.destroyAllWindows()
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+# Safety check
+if len(objpoints) == 0:
+    raise ValueError("No chessboard corners were detected in any images.")
 
-img = cv.imread('left12.jpg')
-h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+# Calibration
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray_shape, None, None)
 
-# undistort
-mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+# Undistort an image
+img = cv.imread('calibrate0.jpg')
+if img is None:
+    raise FileNotFoundError("Could not read 'calibrate0.jpg'.")
+
+h, w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
 dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
- 
-# crop the image
+
+# Crop
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
 cv.imwrite('calibresult.png', dst)
 
-
+# Reprojection error
 mean_error = 0
 for i in range(len(objpoints)):
     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
     mean_error += error
 
-print( "total error: {}".format(mean_error/len(objpoints)) )
-
-
+print("total error: {}".format(mean_error / len(objpoints)))
