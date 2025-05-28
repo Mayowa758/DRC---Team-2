aff290d81928165480e744c1fa384e8ad9dfc642
Mayowa Oyekan <z5592472@ad.unsw.edu.au>
2025-05-29 00:09:40 +1000

    fixed the camera distortion issue

--- configure/undistort_data.py
+++ configure/undistort_data.py
@@ -1,9 +1,8 @@
 import cv2 as cv
 import numpy as np
-import os
 
 # Load calibration data
-data = np.load('camera_calibration.npz')
+data = np.load('configure/camera_calibration.npz')
 mtx = data['camera_matrix']
 dist = data['dist_coeffs']
 newcameramtx = data['new_camera_matrix']
--- road_detect.py
+++ road_detect.py
@@ -3,6 +3,12 @@ import numpy as np
 from util import get_limits
 from configure.undistort_data import *
 
+# data = np.load('configure/camera_calibration.npz')
+# mtx = data['camera_matrix']
+# dist = data['dist_coeffs']
+# newcameramtx = data['new_camera_matrix']
+# roi = data['roi']
+
 # from colour_detect import get_mask
 
 # from colour_detect import mask_blue, mask_yellow
@@ -33,12 +39,16 @@ video.set(cv.CAP_PROP_FRAME_WIDTH, 640)
 video.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 video.set(cv.CAP_PROP_FPS, 30)
 
-h, w = video.shape[:2]
+ret, frame = video.read()
+if not ret or frame is None:
+    raise RuntimeError("Failed to read frame from camera")
+
+h, w = frame.shape[:2]
 mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), cv.CV_16SC2)
+
 
 while True:
     __, img = video.read()
-
     img = cv.GaussianBlur(img, (13, 13), 0)
     img = cv.remap(img, mapx, mapy, interpolation=cv.INTER_LINEAR)
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
