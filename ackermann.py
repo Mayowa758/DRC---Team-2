5cea7c8ea32ba8cc9721c06e390708b3ff339a73
Nicholas Santoso <nicholassantoso@student.unsw.edu.au>
2025-06-08 20:06:38 +1000

    Adding basic code for the ackermann steering.

--- ackermann.py
+++ ackermann.py
@@ -4,17 +4,24 @@ import math
 class ackermann():
     def ackermann_steer_step(x, y, heading_deg, steering_deg, wheelbase, speed, dt):
 
+        #params definition:
+        # x: x-coordinate of the track
+        # y: y-coordinate of the track
+        # heading_deg: current heading
+        # steering_deg: Based on PID error, this is the angle it wants to turn by
+        # wheelbase: Distance between from axel and rear axel wheel
+        # dt time interval between each frame shot
         # If the path is straight, then its angle does not change. Only the x and y positions change
         if steering_deg == 0:
             heading_rad = math.radians(heading_deg)
             x += speed * dt * math.cos(heading_rad)
             y += speed * dt * math.sin(heading_rad)
             return x, y, heading_deg
-        
+
         # Otherwise if the angle is not 0, calcualte the new position.
         steering_rad = math.radians(steering_deg)
 
-        #This calculates the radius of the track
+        # This calculates the radius of the turn
         radius = wheelbase / math.tan(steering_rad)
 
         # Simple angular velocity translation
