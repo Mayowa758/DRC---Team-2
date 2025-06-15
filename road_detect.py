398404325eb272c29fe8652f4816d2fc100b57ba
kevin <z5592178@ad.unsw.edu.au>
2025-06-15 12:55:15 +1000

    Fixed up some code, style, imported ackermann functions and added motor stop function to road_detect.py

--- road_detect.py
+++ road_detect.py
@@ -1,74 +1,11 @@
 import cv2 as cv
 import numpy as np
-import time
-import pigpio
-import Rpi.GPIO as GPIO
-# from gpiozero import AngularServo
 from util import get_limits
 from configure.undistort_data import *
 from colour_detect import *
 from misc_detect import *
-
-# Angle and Pulse constants
-MAX_STEERING_ANGLE = 30
-MIN_STEERING_ANGLE = -30
-PULSE_MIN = 1000
-PULSE_MAX = 2000
-
-# PID constants
-# These values will need to be adjusted
-KP = 0.4    # proportional constant
-KI = 0.01   # integral constant
-KD = 0.2    # derivative constant
-
-# PID state
-integral = 0
-last_error = 0
-
-# Integral limit to prevent windup
-INTEGRAL_MAX = 100
-INTEGRAL_MIN = -100
-
-# Connecting the servo
-SERVO_PIN = 18
-pi = pigpio.pi()
-
-# Connecting the DC motors
-# Left motor
-LEFT_IN1 = 23
-LEFT_IN2 = 24
-LEFT_EN = 18    # PWM pin
-
-# Right motor
-RIGHT_IN1 = 27
-RIGHT_IN2 = 22
-RIGHT_EN = 13   # PWM pin
-
-GPIO.setmode(GPIO.BCM)
-
-# Setup direction pins
-GPIO.setup(LEFT_IN1, GPIO.OUT)
-GPIO.setup(LEFT_IN2, GPIO.OUT)
-GPIO.setup(RIGHT_IN1, GPIO.OUT)
-GPIO.setup(RIGHT_IN2, GPIO.OUT)
-
-# Setup PWM pins
-GPIO.setup(LEFT_EN, GPIO.OUT)
-GPIO.setup(RIGHT_EN, GPIO.OUT)
-
-# Set direction: both motors forward
-GPIO.output(LEFT_IN1, GPIO.HIGH)
-GPIO.output(LEFT_IN2, GPIO.LOW)
-
-GPIO.output(RIGHT_IN1, GPIO.HIGH)
-GPIO.output(RIGHT_IN2, GPIO.LOW)
-
-# Initialise PWM
-left_pwm = GPIO.PWM(LEFT_EN, 1000)  # 1kHz frequency
-right_pwm = GPIO.PWM(RIGHT_EN, 1000)
-left_pwm.start(0)   # Start with 0% duty cycle (stopped)
-right_pwm.start(0)
-
+from ackermann import *
+import time
 
 video = cv.VideoCapture(0)
 window_width = 640
@@ -108,62 +45,6 @@ def get_largest_contour(contours):
     else :
       return None
 
-# This function converts the PID error into a steering angle
-def convert_PID_error_to_steering_angle(error, dt):
-    global integral, last_error
-
-    integral += error * dt
-    integral = max(min(integral, INTEGRAL_MAX), INTEGRAL_MIN)
-    derivative = (error - last_error) / dt
-    last_error = error
-
-    control = KP * error + KI * integral + KD * derivative
-
-    # Clamp angle to (allowed) range
-    steering_angle = max(min(control, MAX_STEERING_ANGLE), MIN_STEERING_ANGLE)
-
-    return steering_angle
-
-# This function allows the steering angle calculated to be actuated on the servo
-def set_servo_angle(angle):
-    # This function maps the steering angle to microseconds (or duty cycle) - servos understand PWM pulses, not angles
-    pulse_width = int(np.interp(angle, [MIN_STEERING_ANGLE, MAX_STEERING_ANGLE], [PULSE_MIN, PULSE_MAX]))
-    pi.set_servo_pulsewidth(SERVO_PIN, pulse_width)
-
-
-##################### Nick's code for setting the servo angle ##########################
-# # Changing this during testing period - The minimum angle required to slow down the back motors
-# MINIMUM_THRESHOLD = 18
-
-
-# # You should change the first argument to the pin where the servo is connected to in the pi.
-# # The 2nd and 3rd arguments are assuming we have a large servo motor.
-# # By default, the angle range is from -90 to 90 degrees
-
-
-# servo = AngularServo(25, min_pulse_width=0.0005, max_pulse_width=0.0025)
-
-# class MotorDrive():
-#     def map_steering_angle_to_servo(angle_from_pid):
-#         while (True):
-#             servo.angle = angle_from_pid
-#             print(f"Steering angle set to: {angle_from_pid: .2f}°")
-#             sleep(0.1)
-#########################################################################################
-
-# This function calculates the speed of the wheels based on the steering angle
-def calculate_speed(steering_angle, max_speed=1.0, min_speed=0.4):
-    angle = abs(steering_angle)
-
-    speed = max_speed - (angle / MAX_STEERING_ANGLE) * (max_speed - min_speed) 
-    return speed
-
-# This function allows the speed calculated to be actuated on the DC motors
-def set_motor_speed(speed):
-    duty = speed * 100
-    left_pwm.ChangeDutyCycle(duty)
-    right_pwm.ChangeDutyCycle(duty)
-
 # This function is reponsible for doing the main road detection
 def road_detection(blue_contour, yellow_contour, transformed_frame, frame):
 
@@ -189,21 +70,6 @@ def road_detection(blue_contour, yellow_contour, transformed_frame, frame):
             frame_center_x = frame_widthx // 2
             error = frame_center_x - center_x
             print(error)
-
-            # Converting error into steering angle using PID control
-            current_time = time.time()
-            dt = current_time - prev_time
-            prev_time = current_time
-
-            # Obtaining steering angle
-            steering_angle = convert_PID_error_to_steering_angle(error, dt)
-
-            # Calculating speed from steering angle
-            speed = calculate_speed(steering_angle)
-
-            # Steering angle and speed implemented on servo motor and DC motors respectively
-            set_servo_angle(steering_angle)
-            set_motor_speed(speed)
 
             # Line to show the Calculated Center
             cv.line(transformed_frame, (cx_blue, cy_blue), (cx_yellow, cy_yellow),
@@ -240,14 +106,14 @@ def finish_line(transformed_frame):
         x, y, w, h = cv.boundingRect(green_area)
         if green_area > 200 and h > 20 and w > frame_x * 0.6 and y > frame_y * 0.7:
             print("We made it to the finish!!")
-            # Stop motor function will be put here
+            stop_motor()
 
 # Change the frame rate of the camera
 video.set(cv.CAP_PROP_FRAME_WIDTH, window_width)
 video.set(cv.CAP_PROP_FRAME_HEIGHT, window_height)
 video.set(cv.CAP_PROP_FPS, 30)
 
-# Setting default error value
+# Setting default initial error value
 error = 0
 
 # Starting timer right before video capture
@@ -292,10 +158,25 @@ def road_detect():
         # Gets contours of blue and yellow masks respectively
         blue_contour, _ = cv.findContours(blue_mask_bv, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         yellow_contour, _ = cv.findContours(yellow_mask_bv, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        
         # Actual Functionality
         error = road_detection(blue_contour, yellow_contour, transformed_frame, hsv_img)
         error = arrow_detection(hsv_img, error)
         error = obstacle_detection(hsv_img, error)
+
+        # Converting error into steering angle using PID control
+        current_time = time.time()
+        dt = current_time - prev_time
+        prev_time = current_time
+
+        # Obtaining steering angle and calculating speed from steering angle
+        steering_angle = convert_PID_error_to_steering_angle(error, dt)
+        speed = calculate_speed(steering_angle)
+
+        # Steering angle and speed implemented on servo motor and DC motors respectively
+        set_servo_angle(steering_angle)
+        set_motor_speed(speed)
+
         finish_line(transformed_frame)
 
         # cv.imshow('before', prev)
@@ -304,11 +185,9 @@ def road_detect():
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
-    pi.set_servo_pulsewidth(SERVO_PIN, 0)
-    pi.stop()
-    left_pwm.stop()
-    right_pwm.stop()
-    GPIO.cleanup()
+    close_servo()
+    close_motor()
+    cleanup_GPIO()
     
     video.release()
     cv.destroyAllWindows()
