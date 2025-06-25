457dcdc090a96a24962ace666be5ccc413de3cd2
kevinhuangprogramming <kevjiyuan@gmail.com>
2025-06-25 17:55:17 +1000

    Update road_detect.py

--- road_detect.py
+++ road_detect.py
@@ -118,8 +118,6 @@ def finish_line(transformed_frame):
         x, y, w, h = cv.boundingRect(green_area)
         if h > 20 and w > frame_x * 0.4 and y > frame_y * 0.7:
             print("We made it to the finish!!")
-            stop_motors()
-            stop_servo()
             return True
     return False
 
@@ -200,9 +198,7 @@ def road_detect():
                     break
                 elif key == ord('q'):
                     print("Exiting program...")
-                    turn_off_servo()
-                    turn_off_motors()
-                    cleanup_GPIO()
+                    shutdown()
                     video.release()
                     cv.destroyAllWindows()
                     return
@@ -235,6 +231,8 @@ def road_detect():
         set_motor_speed(speed)
 
         if finish_line(transformed_frame):
+            stop_motors()
+            stop_servo()
             finished = True
             started = False
             continue
