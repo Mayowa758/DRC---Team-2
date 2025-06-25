4873c18fccfc09ccad8e4cfa351bde01657b8ada
Mayowa Oyekan <mayowa.oyekan@outlook.com>
2025-06-25 20:04:03 +1000

    changes

--- servo.py
+++ servo.py
@@ -6,12 +6,12 @@ import time
 
 #################################### Defining servo and DC motor connection pins ################################################
 # Defining servo motor pin
-SERVO_PIN = 25
+SERVO_PIN = 12
 servo = AngularServo(
         pin=SERVO_PIN,
-        min_angle=MIN_STEERING_ANGLE,
-        max_angle=MAX_STEERING_ANGLE,
-        min_pulse_width=PULSE_MIN,  # 0.001
-        max_pulse_width=PULSE_MAX   # 0.002
+        min_angle=-50,
+        max_angle=50,
+        min_pulse_width=0.001,  # 0.001
+        max_pulse_width=0.002   # 0.002
     )
 servo.angle = 0
