6b5199925b0c6c489440b041210e055f7dacd009
Mayowa Oyekan <z5592472@ad.unsw.edu.au>
2025-06-25 20:00:12 +1000

    added some different servo code

--- servo.py
+++ servo.py
@@ -1,17 +1,8 @@
-import numpy as np
-from gpiozero import AngularServo, PWMOutputDevice, DigitalOutputDevice
-import math
-import time
+from gpiozero import Servo
+from time import sleep
+from gpiozero.pins.pigpio import PiGPIOFactory
 
+factory = PiGPIOFactory()
 
-#################################### Defining servo and DC motor connection pins ################################################
-# Defining servo motor pin
-SERVO_PIN = 25
-servo = AngularServo(
-        pin=SERVO_PIN,
-        min_angle=MIN_STEERING_ANGLE,
-        max_angle=MAX_STEERING_ANGLE,
-        min_pulse_width=PULSE_MIN,  # 0.001
-        max_pulse_width=PULSE_MAX   # 0.002
-    )
-servo.angle = 0
+servo = Servo(12, pin_factory=factory)
+servo.mid()
