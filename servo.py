4d857bb5775bf091fc1ec31f05912622af13f88f
Mayowa Oyekan <z5592472@ad.unsw.edu.au>
2025-06-25 20:02:18 +1000

    added

--- servo.py
+++ servo.py
@@ -5,4 +5,18 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 factory = PiGPIOFactory()
 
 servo = Servo(12, pin_factory=factory)
+
+print("Start in middle")
 servo.mid()
+sleep(1)
+print("Go to min")
+servo.min()
+sleep(1)
+print("Go to max")
+servo.max()
+sleep(1)
+print("back to middle")
+servo.mid()
+sleep(1)
+
+print("end")
