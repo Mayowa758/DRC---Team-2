9a0e53b6bb3a20709bc71b124d112b6a0f7dc5b6
Mayowa Oyekan <mayowa.oyekan@outlook.com>
2025-06-27 01:39:41 +1000

    testing code

--- servo3.py
+++ servo3.py
@@ -1,27 +1,31 @@
 from gpiozero import Servo
 from time import sleep
 
-SERVO_PIN = 18  # GPIO18, physical pin 12
+SERVO_PIN = 18  # GPIO18 (physical pin 12)
 
+# Create servo object with accurate pulse width limits
 servo = Servo(
     SERVO_PIN,
-    min_pulse_width=0.0009,  # 0.5 ms
-    max_pulse_width=0.0021   # 2.5 ms
+    min_pulse_width=0.00107,  # 1070 �s
+    max_pulse_width=0.00197   # 1970 �s
 )
 
 try:
     while True:
-        servo.value = -0.25
-        print("Angle: -50�")
+        # Move to ~-50�
+        servo.value = -1.0
+        print("Position: ~-50�")
         sleep(2)
-        
-        servo.value = 0
-        print("Angle: 0�")
+
+        # Move to center (0�)
+        servo.value = 0.0
+        print("Position: 0� (center)")
         sleep(2)
-        
-        servo.value = 0.25
-        print("Angle: 50�")
+
+        # Move to ~+50�
+        servo.value = 1.0
+        print("Position: ~+50�")
         sleep(2)
 
 except KeyboardInterrupt:
-    print("Program stopped")
+    print("\nTest stopped by user.")
